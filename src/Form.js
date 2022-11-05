c8ab640da4ff8981aba4fbcf39ff4c8213b7950e
Oluwanifemi <temenuoluwanifemi2005@gmail.com>
2022-11-05 14:04:34 +0100

    Styled all elements appropriately in the contact page

--- src/Form.js
+++ src/Form.js
@@ -5,20 +5,18 @@ export const Form =()=>{
                 <FormText/>
                 <div>
                     <label for="email">Email</label>
-                    <input type="email" required placeholder="yourname@email.com" id="email" class="input"/>
+                    <input type="email" required placeholder="yourname@email.com" id="email" className="input"/>
                 </div>
                 <div class="form-group">
                     <label for="Message">Message</label>
                     <textarea type="text" required placeholder="Send me a message and I'll reply you as soon as possible..." id="messgae" class="user" rows="5"></textarea>
                 </div>
-                <div class="checkbox">
-                    <input type="checkbox" name="" id=""/>
-                    <p>You agree to providing your data to nifemi who may contact you.</p>
+                <div className="checkbox">
+                    <input type="checkbox" name="" id="checkbox"/>
+                    <p>You agree to providing your data to Niffyboy who may contact you.</p>
                 </div>
-                <div class="">
-                    <a href="">
-                        <input type="submit" class="submit" value="Send Message" id="btn__submit"/>
-                    </a>
+                <div className="">
+                        <input type="submit" className="submit" value="Send Message" id="btn__submit"/>
                 </div>
             </form>
         );
